cd7adf570b8fb5ef1ce5a9401c43520eb45144f3
Prakhar Singh <prakhar22153054@akgec.ac.in>
2024-10-28 22:00:23 +0530

    Refactor server/index.js to use environment variables for port configuration

--- server/index.js
+++ server/index.js
@@ -1,15 +1,10 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config({});
 
 const app = express();
-
-app.get("/home",(req, res)=>{
-    return res.status(200).json({
-        message:"I am coming from backend",
-        success:true
-    })
-})
 
 //middleware
 app.use(express.json())
@@ -23,7 +18,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT,()=>{
     console.log(`server running at port ${PORT}`);  
