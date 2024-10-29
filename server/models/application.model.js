af6dbe49a6121c49e579f3ede477955a42b10eda
Prakhar Singh <prakhar22153054@akgec.ac.in>
2024-10-29 13:16:50 +0530

    Refactor application model to include job, applicant, and status fields

--- server/models/application.model.js
+++ server/models/application.model.js
@@ -1,2 +1,21 @@
 import mongoose from "mongoose";
 
+const applicationSchema = new mongoose.Schema({
+    job:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:'Job'
+    },
+    applicant:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:'User'
+    },
+    status:{
+        type:String,
+        enum:['pending','accepted','rejected'],
+        default:pending
+    }
+},{timeseries:true});
+
+export const Application = mongoose.model("Application", applicationSchema);
