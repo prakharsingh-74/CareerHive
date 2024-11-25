bd0338b7ad248854f3a94234896a45b32e6ac52a
Prakhar Singh <prakhar22153054@akgec.ac.in>
2024-11-25 10:48:02 +0530

    Update API endpoint URLs to point to the new production environment

--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -1,9 +1,9 @@
-export const USER_API_END_POINT="https://careerhive-a1ft.onrender.com/api/v1/user";
-export const JOB_API_END_POINT="https://careerhive-a1ft.onrender.com/api/v1/job";
-export const APPLICATION_API_END_POINT="https://careerhive-a1ft.onrender.com/api/v1/application";
-export const COMPANY_API_END_POINT="https://careerhive-a1ft.onrender.com/api/v1/company";
+//export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
+//export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
+//export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
+//export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
 
-// export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
-// export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
-// export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
-// export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
+export const USER_API_END_POINT="http://https://careerhive-0elt.onrender.com/api/v1/user";
+export const JOB_API_END_POINT="http://https://careerhive-0elt.onrender.com/v1/job";
+export const APPLICATION_API_END_POINT="http://https://careerhive-0elt.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT="http://https://careerhive-0elt.onrender.com/api/v1/company";
